de6e58b9ef0d73f81da9d8a0556a1690e71ffc3a
FunkyDm <mrforxbox@mail.ru>
2025-06-10 15:45:05 +0300

    in progress

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -1,5 +1,12 @@
 package ru.skypro.homework.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.skypro.homework.dto.UserDTO;
+import ru.skypro.homework.model.UserEntity;
 
+@Mapper
 public interface UserMapper {
+    @Mapping(source = "")
+    UserDTO userEntityToUserDTO(UserEntity userEntity);
 }
