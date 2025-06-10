26993443b5383c3cf1a5b1453ac402cd465687e4
FunkyDm <mrforxbox@mail.ru>
2025-06-10 15:25:39 +0300

    Созданы сущности и репозитории

--- src/main/java/ru/skypro/homework/model/UserEntity.java
+++ src/main/java/ru/skypro/homework/model/UserEntity.java
@@ -1,4 +1,33 @@
 package ru.skypro.homework.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private String role;
+
+    private String image;
+
 }
