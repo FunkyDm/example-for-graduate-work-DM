c7a4379caf3aafbd79b0365bec3438a0d829f1cc
FunkyDm <mrforxbox@mail.ru>
2025-06-11 14:46:51 +0300

    Созданы классы мапперов с необходимыми методами

--- src/main/java/ru/skypro/homework/mapper/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdMapper.java
@@ -1,4 +1,16 @@
 package ru.skypro.homework.mapper;
 
-public class AdMapper {
+import org.mapstruct.Mapper;
+import ru.skypro.homework.dto.AdDTO;
+import ru.skypro.homework.dto.CreateOrUpdateAdDTO;
+import ru.skypro.homework.dto.ExtendedAdDTO;
+import ru.skypro.homework.model.AdEntity;
+
+@Mapper
+public interface AdMapper {
+    AdDTO adEntityToAdDTO(AdEntity adEntity);
+
+    ExtendedAdDTO adEntityToExtendedAdDTO(AdEntity adEntity);
+
+    AdEntity createOrUpdateAdDTOToAdEntity(CreateOrUpdateAdDTO createOrUpdateAdDTO);
 }
--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -2,11 +2,28 @@ package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import ru.skypro.homework.dto.RegisterDTO;
+import ru.skypro.homework.dto.UpdateUserDTO;
 import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.model.UserEntity;
 
 @Mapper
 public interface UserMapper {
-    @Mapping(source = "")
     UserDTO userEntityToUserDTO(UserEntity userEntity);
+
+    @Mappings(value = {
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "email", source = "username"),
+            @Mapping(target = "image", ignore = true)
+    })
+    UserEntity registerDTOToUserEntity(RegisterDTO registerDTO);
+
+    @Mappings(value = {
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "email", source = "username"),
+            @Mapping(target = "role", ignore = true),
+            @Mapping(target = "image", ignore = true)
+    })
+    UserEntity updateUserDTOToUserEntity(UpdateUserDTO updateUserDTO);
 }
